7fd28ba8460afb41ed780d09f3278cfea94b660b
abk <abobkar96@gmail.com>
2022-12-15 17:05:27 +0300

    Back Ground Img path update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ function Nav(props) {
   const step = props.step ;
 
   return (
-    <nav className="md:w-[274px] md:h-full md:bg-[url('./src/assets/bg-sidebar-desktop.svg')] bg-[url('./src/assets/bg-sidebar-mobile.svg')] bg-cover md:bg-bottom bg-center bg-no-repeat  md:rounded-2xl md:py-5 py-10 px-5 min-h-[175px]" >
+    <nav className="md:w-[274px] md:h-full md:bg-[url('./assets/bg-sidebar-desktop.svg')]  bg-[url('./assets/bg-sidebar-mobile.svg')] bg-cover md:bg-bottom bg-center bg-no-repeat  md:rounded-2xl md:py-5 py-10 px-5 min-h-[175px]" >
     <ul className="flex md:flex-col w-full md:h-full gap-5 h-fit  justify-center md:justify-start">
       <li className="flex items-center  gap-2" >
         <div className={`rounded-full text-black  w-[30px] grid  h-[30px] place-content-center  border border-pastelBlue text-sm font-normal ${step === 1 ? 'bg-pastelBlue text-black' :'text-white'}`}>1</div>
